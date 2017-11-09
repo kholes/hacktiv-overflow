c818ed452483309f3fed5565c444d2c2c5faea9f
kholes <cholis.elbazar@gmail.com>
2017-11-09 20:03:18 +0700

    update mongodb to mongo atlas

--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/dboverflow')
+mongoose.connect('mongodb://javas:1qs2wd3ef@cluster0-shard-00-00-xi6pm.mongodb.net:27017,cluster0-shard-00-01-xi6pm.mongodb.net:27017,cluster0-shard-00-02-xi6pm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 let Schema = mongoose.Schema, ObjectId = Schema.ObjectId
 let usersSchema = new Schema({
     username:{ 
